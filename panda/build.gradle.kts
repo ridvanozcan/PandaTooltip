67b36589294495c9a253cc59cf11b44cffc47af0
Ridvan Ozcan <Ridvan.Ozcan@sahibinden.com>
2024-04-22 12:38:29 +0300

    update version

--- panda/build.gradle.kts
+++ panda/build.gradle.kts
@@ -65,7 +65,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.github.ridvanozcan"
                 artifactId = "pandatooltip"
-                version = "1.0.5"
+                version = "1.0.6"
 
                 from(components["release"])
             }
